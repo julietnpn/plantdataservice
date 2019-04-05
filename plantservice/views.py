a8f362c665dae0045d106f655b78fb05968c4e23
Michael Stenta <mike@mstenta.net>
2019-04-05 11:27:12 -0400

    Whitespace.

--- plantservice/views.py
+++ plantservice/views.py
@@ -5,8 +5,10 @@ from flask_admin.contrib.sqla import ModelView
 class PlantView(ModelView):
     """Extend the ModelView class for Plant models."""
 
+
 class FamilyView(ModelView):
     """Extend the ModelView class for Family models."""
+
 
 class GenusView(ModelView):
     """Extend the ModelView class for Genus models."""
